902a232b8436df172c1a49120e9894bcbcf577f3
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-28 15:16:40 +0300

    method for writing N loglines to a file, within a fixed frequency is properly implemented. ready for actual testing

--- Conf/update_logfile.py
+++ Conf/update_logfile.py
@@ -2,6 +2,9 @@ import os
 from numpy import random as rd
 import time
 
+backup_logfile = open(
+    '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial_backup.log', 'r')
+
 
 def writeLines(number, file):
     for _ in range(number):
@@ -10,14 +13,25 @@ def writeLines(number, file):
         file.write(random_line)
 
 
+# The path to the logfile that must be constantly update
 file = open('/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial.log', 'r+')
 
+# Store the initial log-file content into an array of lines
 lines = file.readlines()
 
-for _ in range(5):
-    print('writing to file...')
-    writeLines(150, file)
-    time.sleep(30)
 
+def LogLineWriter(file, nLines, nReps):
+    logstash_init_time = 2
+    lines_beat_time = 1
+    print(f'⏳Wait for the logstash instance to start...')
+    time.sleep(logstash_init_time)
+    for _ in range(nReps):
+        print(f'Writing an additional {nLines} lines to the logfile...📑 ')
+        # writeLines(nLines, file)
+        time.sleep(lines_beat_time)
+    print(f'Finished writing the entire log batch to the logfile...')
+
+
+LogLineWriter(file, 150, 5)
 
 file.close()
