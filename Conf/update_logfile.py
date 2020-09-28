4aa71766029ff539a0014872aa57adf97f3535bc
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-28 16:38:48 +0300

    Adds method for periodically send loglines to a file. issue with resetting the content and then write over

--- Conf/update_logfile.py
+++ Conf/update_logfile.py
@@ -2,36 +2,49 @@ import os
 from numpy import random as rd
 import time
 
-backup_logfile = open(
-    '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial_backup.log', 'r')
+backup_logfile = '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial_backup.log'
 
 
-def writeLines(number, file):
+def ResetFile(backup_file, file):
+    with open(backup_file) as f:
+        lines = f.readlines()
+        with open(file, 'w') as ff:
+            print(
+                f'Resetting the logfile to its standard beat content due to increased size...')
+            for line in lines:
+                ff.write(line)
+                # print(line)
+
+
+def WriteLines(file, lines, number):
+    # file.write('\n')
     for _ in range(number):
         random_line = rd.choice(lines).strip()+'\n'
-        # print(random_line)
-        file.write(random_line)
+        # print(random_line.strip())
+        file.write('\n'+random_line.strip())
 
 
 # The path to the logfile that must be constantly update
-file = open('/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial.log', 'r+')
+logfile = '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial.log'
 
-# Store the initial log-file content into an array of lines
-lines = file.readlines()
+# Use this method to reset the contents of the initial logbeat file
+# ResetFile(backup_logfile, logfile)
 
 
 def LogLineWriter(file, nLines, nReps):
     logstash_init_time = 2
-    lines_beat_time = 1
+    lines_beat_time = 5
     print(f'⏳Wait for the logstash instance to start...')
     time.sleep(logstash_init_time)
-    for _ in range(nReps):
-        print(f'Writing an additional {nLines} lines to the logfile...📑 ')
-        # writeLines(nLines, file)
-        time.sleep(lines_beat_time)
+    with open(file, 'r+') as logfile:
+        lines = logfile.readlines()
+        if(len(lines) > 500):
+            ResetFile(backup_logfile, file)
+        for _ in range(nReps):
+            print(f'Writing an additional {nLines} lines to the logfile...📑 ')
+            WriteLines(logfile, lines, nLines)
+            time.sleep(lines_beat_time)
     print(f'Finished writing the entire log batch to the logfile...')
 
 
-LogLineWriter(file, 150, 5)
-
-file.close()
+LogLineWriter(logfile, 100, 2)
