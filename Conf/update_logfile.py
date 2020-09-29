a5c32e36c63c1451089b1fdd5a4da48ec55a94b4
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-29 08:30:05 +0300

    adds method that periodically writes logs into a given file. conditions for empy file and too large file are also implemented

--- Conf/update_logfile.py
+++ Conf/update_logfile.py
@@ -5,46 +5,70 @@ import time
 backup_logfile = '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial_backup.log'
 
 
+# The path to the logfile that must be constantly update
+logfile = '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial.log'
+
+
 def ResetFile(backup_file, file):
-    with open(backup_file) as f:
+    with open(backup_file, 'r') as f:
         lines = f.readlines()
         with open(file, 'w') as ff:
             print(
-                f'Resetting the logfile to its standard beat content due to increased size...')
+                f'🔄 Resetting the logfile to its standard beat content due to increased size...')
             for line in lines:
                 ff.write(line)
-                # print(line)
+
+
+def FillFile(backup_file, file):
+    with open(backup_file, 'r') as f:
+        lines = f.readlines()
+        with open(file, 'w') as ff:
+            print(
+                f'⤵️Filling the logfile with backed up data due to non-existent data...')
+            for line in lines:
+                ff.write(line)
+
+
+def Reset_OTF(backup_file, file):
+    backup = open(backup_file, 'r')
+    lines = backup.readlines()
+    print(f'🔄 Resetting the logfile to its standard beat content due to increased size...')
+    for line in lines:
+        file.write(line)
+        # print(line)
+    backup.close()
 
 
 def WriteLines(file, lines, number):
     # file.write('\n')
-    for _ in range(number):
-        random_line = rd.choice(lines).strip()+'\n'
-        # print(random_line.strip())
-        file.write('\n'+random_line.strip())
+    with open(file, 'a') as f:
+        # f.write('\n')
+        for _ in range(number):
+            random_line = rd.choice(lines).strip()+'\n'
+            # print(random_line.strip())
+            f.write(random_line.strip()+'\n')
 
-
-# The path to the logfile that must be constantly update
-logfile = '/Users/basavyr/Library/Mobile Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/ELK-Stack-macOS/Resources/logstash-tutorial.log'
 
 # Use this method to reset the contents of the initial logbeat file
 # ResetFile(backup_logfile, logfile)
 
 
 def LogLineWriter(file, nLines, nReps):
-    logstash_init_time = 2
-    lines_beat_time = 5
+    logstash_init_time = 1
+    lines_beat_time = 8
     print(f'⏳Wait for the logstash instance to start...')
     time.sleep(logstash_init_time)
-    with open(file, 'r+') as logfile:
-        lines = logfile.readlines()
-        if(len(lines) > 500):
-            ResetFile(backup_logfile, file)
-        for _ in range(nReps):
-            print(f'Writing an additional {nLines} lines to the logfile...📑 ')
-            WriteLines(logfile, lines, nLines)
-            time.sleep(lines_beat_time)
-    print(f'Finished writing the entire log batch to the logfile...')
+    lines = open(file, 'r').readlines()
+    if(not len(lines)):
+        FillFile(backup_logfile, file)
+    elif len(lines) > 1500:
+        ResetFile(backup_logfile, file)
+    for _ in range(nReps):
+        lines = open(file, 'r').readlines()
+        print(f'Writing an additional {nLines} lines to the logfile...📑 ')
+        WriteLines(file, lines, nLines)
+        time.sleep(lines_beat_time)
+    print(f'⌛️ Finished writing the entire log batch to the logfile...')
 
 
-LogLineWriter(logfile, 100, 2)
+LogLineWriter(logfile, 100, 1)
