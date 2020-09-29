d272a0d6833a10591aac5470300658f18dd13f28
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-09-29 12:17:33 +0300

    Adds closeer for any logstash instances

--- Conf/close_logstash.py
+++ Conf/close_logstash.py
@@ -1,5 +1,6 @@
 import platform
 import subprocess
+import time
 
 current_system = platform.uname().node
 
@@ -8,7 +9,17 @@ pids = pid_file.readlines()
 print(
     f'There are {len(pids)} logstash instances 🔄 running currently on 💻 {current_system}')
 
+command = 'ps aux | grep logstash'
+subprocess.call(command, shell=True)
+
+idle_time=60
+
+time.sleep(idle_time)
+
 for PID in pids:
     command = f'kill -9 {PID}'
     print(f'❌ Stopping process {PID}')
     subprocess.call(command, shell=True)
+
+command = 'ps aux | grep logstash'
+subprocess.call(command, shell=True)
